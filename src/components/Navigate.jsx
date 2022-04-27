38fe497f4babf2a84be493822fbfb58e42d0bcea
Yosua Nicolaus <cryodawn257@gmail.com>
2022-04-27 09:28:57 +0700

    create and style Navbar
    using reactstrap's component.

--- src/components/Navigate.jsx
+++ src/components/Navigate.jsx
@@ -1,8 +1,43 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+} from "reactstrap";
+
 function Navigate() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
-    <>
-      <div className="lead">Nav tabs & bar here</div>
-    </>
+    <Navbar expand="md" dark color="primary">
+      <NavbarBrand>Elegant</NavbarBrand>
+      <NavbarToggler onClick={toggle} className="shadow" />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav navbar className="w-100">
+          <NavItem className="ms-auto">
+            <Link to={"/"} className="nav-link">
+              Home
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link to={"/"} className="nav-link">
+              Shop
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link to={"/"} className="nav-link">
+              Cart
+            </Link>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>
   );
 }
 
