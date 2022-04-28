8fa530f8572ebfe3daac790d62755cd450390799
Yosua Nicolaus <cryodawn257@gmail.com>
2022-04-28 08:49:36 +0700

    add github link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,15 @@
 function Footer() {
   return (
     <div className="mt-auto">
-      <div className="bg-black text-center lead">
-        copyright © yosuanicolaus 2022
+      <div className="bg-black text-center lead p-1">
+        <a
+          href="https://github.com/yosuanicolaus/shopping-cart"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link-light opacity-25"
+        >
+          copyright © yosuanicolaus 2022
+        </a>
       </div>
     </div>
   );
